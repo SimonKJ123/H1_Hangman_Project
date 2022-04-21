a321557ad3c14e8587b93b705b54432f52597fc7
Simon Karas-Jensen <simo754p@elev.tec.dk>
2022-04-21 08:46:57 +0200

    Add project files.

---
+++ H1_Hangman_Project/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Random;
+using System.Text;
+
+namespace HangmanGame
+{
+    internal class Program
+    {
+        private static void printhangman(int wrong)
+        {
+            if (wrong == 0)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+
+            }
+            else if (wrong == 1)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("    |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+            }
+            else if (wrong == 2)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine(" |  |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+            }
+            else if (wrong == 3)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|  |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+            }
+            else if (wrong == 4)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("    |");
+                Console.WriteLine("   ===");
+            }
+            else if (wrong == 5)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("/   |");
+                Console.WriteLine("   ===");
+            }
+            else if (wrong == 6)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O  |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("/ \\  |");
+                Console.WriteLine("   ===");
+            }
+        }
+
+        private static int printword(List<char> lettersguessed, String randomword)
+        {
+            int counter = 0;
+            int correctletter = 0;
+            Console.Write("\r\n");
+            foreach (char c in randomword)
+            {
+                if (lettersguessed.Contains(c))
+                {
+                    Console.Write(c + " ");
+                    correctletter++;
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+                counter++;
+            }
+            return correctletter;
+                        
+        }
+        private static void printLine(String randomword)
+        {
+            Console.Write("\r");
+            foreach(char c in randomword)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                Console.Write("\u0305");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Velkomen til Hangman!");
+            Console.WriteLine("-------------------------------------------");
+
+        }
+    }
+
+
+}
